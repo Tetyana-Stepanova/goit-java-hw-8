2c1e5481b926dbe7c767c3e72d3d8e94aac26fea
Tetyana-Stepanova <95179655+Tetyana-Stepanova@users.noreply.github.com>
2022-01-28 14:38:21 +0200

    Update MyLinkedList.java

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,5 +1,4 @@
-import java.util.*;
-public class Node<T> {
+class Node<T> {
     private T value;
     private Node<T> next;
     private Node<T> previous;
@@ -111,31 +110,45 @@ public class MyLinkedList<T> {
         removeElement = head;
         return result.getValue();
     }
+
+    @Override
+    public String toString(){
+        String result = "[" + head.getValue();
+        Node<T> current = head.getNext();
+        while (current != null){
+            result += "," + current.getValue();
+            current = current.getNext();
+        }
+        result += "]";
+        return result;
+    }
 }
 
-public class MyLinkedListTests {
-        public static void main(String[] args) {
-            MyLinkedList<String> list = new MyLinkedList<>();
+class MyLinkedListTests {
+    public static void main(String[] args) {
+        MyLinkedList<String> list = new MyLinkedList<>();
 
-            System.out.println("list.size() = " + list.size());
+        System.out.println("list.size() = " + list.size());
 
-            list.add("Alfa");
-            System.out.println("list.size() = " + list.size());
+        list.add("Alfa");
+        System.out.println("list.size() = " + list.size());
 
-            list.add("Beta");
-            System.out.println("list.size() = " + list.size());
+        list.add("Beta");
+        System.out.println("list.size() = " + list.size());
 
-            list.add("Gamma");
-            System.out.println("list.size() = " + list.size());
+        list.add("Gamma");
+        System.out.println("list.size() = " + list.size());
 
-            System.out.println("list.get(0) = " + list.get(0));
-            System.out.println("list.get(1) = " + list.get(1));
-            System.out.println("list.get(2) = " + list.get(2));
+        System.out.println("list.get(0) = " + list.get(0));
+        System.out.println("list.get(1) = " + list.get(1));
+        System.out.println("list.get(2) = " + list.get(2));
 
-            System.out.println("removeElement = " + list.remove(1));
-            System.out.println("list.size() = " + list.size());
+        System.out.println(list);
 
-            list.clear();
-            System.out.println("cleanlist.size() = " + list.size());
-        }
+        System.out.println("removeElement = " + list.remove(1));
+        System.out.println("list.size() = " + list.size());
+
+        list.clear();
+        System.out.println("cleanlist.size() = " + list.size());
     }
+}
